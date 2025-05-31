41cfab3ae3c1b6bee71e617b7917f6662443828c
herreraflavio <lurker@flavioherrera.com>
2025-05-30 21:03:08 -0700

    clean code

--- src/app/components/polygons/Box.ts
+++ src/app/components/polygons/Box.ts
@@ -1,14 +1,3 @@
-// export default function Box(): number[][] {
-//   const rings = [
-//     [-120.31, 37.31],
-//     [-120.29, 37.31],
-//     [-120.29, 37.29],
-//     [-120.31, 37.29],
-//     [-120.31, 37.31],
-//   ];
-
-//   return rings;
-// }
 export default function Box(): number[][] {
   const rings = [
     [-120.424817, 37.365371], // top-left
