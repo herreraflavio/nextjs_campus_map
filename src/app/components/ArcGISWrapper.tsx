e9a6c69246e239ec4416d74f9d460525dd3f5a0b
herreraflavio <lurker@flavioherrera.com>
2026-01-05 23:02:43 -0800

    updated api endpoint url

--- src/app/components/ArcGISWrapper.tsx
+++ src/app/components/ArcGISWrapper.tsx
@@ -136,7 +136,8 @@ const DEFAULT_SETTINGS: ExportBody["settings"] = {
 /** fallback external event endpoints */
 const DEFAULT_EVENT_SOURCES: string[] = [
   //"https://uc-merced-campus-event-api-backend.onrender.com/get/events",
-  "https://uc-merced-campus-event-api-backend.onrender.com/presence_events",
+  "https://api.ucmercedhub.com/crimelogs",
+  // "https://uc-merced-campus-event-api-backend.onrender.com/presence_events",
   // "http://127.0.0.1:8050/presence_events",
 ];
 
