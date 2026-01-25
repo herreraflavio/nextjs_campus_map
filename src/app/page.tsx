5e780cb4279c9457170eaca68d64e0445e8b3807
herreraflavio <lurker@flavioherrera.com>
2026-01-25 15:39:42 -0800

    code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
 import { auth } from "@/lib/auth";
-import LoggedInDashboard from "./components/LoggedInDashboard";
-import AuthForm from "./components/AuthForm"; // login + register
 import Home from "@/app/components/Home";
 import LandingPage from "./components/LandingPage";
 export default async function HomePage() {
   const session = await auth();
 
   if (!session) {
-    return (
-      // <div
-      //   style={{
-      //     backgroundImage:
-      //       'url("https://news.ucmerced.edu/sites/news.ucmerced.edu/files/news/image/morning_190708-4.gif")',
-      //     backgroundSize: "cover",
-      //     width: "100%",
-      //     height: "100%",
-      //   }}
-      // >
-      //   <AuthForm />
-      // </div>
-      <LandingPage />
-    );
+    return <LandingPage />;
   }
 
-  //  return <LoggedInDashboard user={session.user} />;
   return <Home user={session.user} />;
 }
