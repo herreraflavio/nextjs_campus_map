fceccdbdac7636a62aea60525a31c1d9c8d16534
herreraflavio <lurker@flavioherrera.com>
2026-03-09 14:40:39 -0700

    adde page

---
+++ src/app/page/[id]/page.tsx
@@ -0,0 +1,20 @@
+import LoggedInDashboard from "@/app/components/LoggedInDashboard";
+
+import { MapProvider } from "@/app/context/MapContext";
+import ArcGISWrapper from "@/app/components/ArcGISWrapper";
+
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
+
+  return (
+    <MapProvider mapId={id}>
+      <div>header</div>
+      <ArcGISWrapper />
+      <div>footer</div>
+    </MapProvider>
+  );
+}
